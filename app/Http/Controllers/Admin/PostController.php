15ec42526f74d5978defb8d990d7e327e60faf3a
Al-E-00 <mazzellaalessio6@gmail.com>
2022-08-09 15:37:01 +0200

    Added find for the slug into PostController

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -80,9 +80,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
-        $post = Post::findOrFail($id);
+        $post = Post::where('slug', $slug)->first();
 
         return view('admin.posts.show', compact('post'));
     }
