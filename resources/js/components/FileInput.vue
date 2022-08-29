d56f865730770f44d9c093a8f04eb52ab87d3cbc
Al-E-00 <mazzellaalessio6@gmail.com>
2022-08-29 15:19:26 +0200

    File input created

---
+++ resources/js/components/FileInput.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="form-group">
+        <label :for="name + '_file'" class="form-label">Immagine di copertina</label>
+
+        <div class="d-flex">
+            <img id="new_cover_img_container" style="max-width: 250px;" :src="thumbnailLink" alt="">
+
+            <input type="file" :name="name" class="form-control-file " @change="onFileChange" :id="name + '_file'">
+        </div>
+        <!--  @error('cover_img')
+    <div class="invalid-feedback">{{ $message }}</div>
+    @enderror -->
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
